87a7fbce8a0632fa3d54ccac3aa0310c8781c8be
Shu-Outstndrs <shu.watanabe111@gmail.com>
2023-07-21 20:34:38 +0900

    headerAddArrow

--- app/routingView.jsx
+++ app/routingView.jsx
@@ -10,18 +10,61 @@ export function RoutingView() {
   return (
     <div className="flex fixed z-30 w-screen p-4 text-xl bg-white/90 backdrop-blur-sm rounded-b-lg">
       <Link
-        className="text-blue-600 hover:text-blue-700 hover:underline"
+        className="flex text-blue-600 hover:text-blue-700 hover:underline"
         href="/"
       >
+        <svg
+          class="w-3 h-3 mt-2 mr-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+        </svg>
         Home
       </Link>
-      <span className="ml-2">/</span>
+      <span className="ml-2">
+        <svg
+          className="w-3 h-3 mt-2 text-gray-400"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 6 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 9 4-4-4-4"
+          />
+        </svg>
+      </span>
       {routePath.map((path, index) => (
         <span
           key={index}
-          className="ml-2 text-blue-600 hover:text-blue-700 hover:underline"
+          className="flex ml-2 text-blue-600 hover:text-blue-700 hover:underline"
         >
-          {index !== 0 && <span> / </span>}
+          {index !== 0 && (
+            <span>
+              <svg
+                className="w-3 h-3 mt-2 mx-1 mr-2 text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </span>
+          )}
           <Link href={`/${routePath.slice(0, index + 1).join("/")}`}>
             {path}
           </Link>
