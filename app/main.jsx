bdcf9086e155e6c65d604e43d20af6c8a7d01cf6
Shu-Outstndrs <shu.watanabe111@gmail.com>
2023-07-21 20:15:25 +0900

    lightModeFix

--- app/main.jsx
+++ app/main.jsx
@@ -29,14 +29,10 @@ export function Main({
         {age ? (
           <>
             <div className="flex justify-between mb-1">
-              <span className="text-base font-medium text-blue-700 dark:text-white">
-                Age
-              </span>
-              <span className="text-sm font-medium text-blue-700 dark:text-white">
-                14%
-              </span>
+              <span className="text-base font-medium text-white">Age</span>
+              <span className="text-sm font-medium text-white">14%</span>
             </div>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <div className="w-full bg-gray-700 rounded-full h-2.5 ">
               <div className="w-[14%] bg-blue-600 h-2.5 rounded-full"></div>
             </div>
           </>
